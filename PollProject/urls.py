5d333e4e894ed46e491f36ed3aada3fa5f717743
Prentice Wongvibulsin <echo0101@gmail.com>
2011-10-22 18:28:26 -0700

    Initial add Django project

---
+++ PollProject/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'PollProject.views.home', name='home'),
+    # url(r'^PollProject/', include('PollProject.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
